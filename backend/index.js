3051ab7523cc721eba4bd23a52c7c1430c5ed3f0
chitrapg <kishanurankar@gmail.com>
2025-02-14 23:40:01 +0530

    All commit

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "*",
+      "https://thealteroffice-client.vercel.app/",
       "http://localhost:3000",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -30,7 +30,7 @@ app.use(
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "*"
+    "https://thealteroffice-client.vercel.app/"
   ); // Update with specific origin if needed
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
